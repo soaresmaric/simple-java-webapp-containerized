149e646770fa951d92827420b52463db0287f078
soaresmaric <maria.costa@ccc.ufcg.edu.br>
2024-03-23 18:42:26 -0300

    Correções pra pegar os repositorios corretamente

--- simple-java-webapp-containerized/src/main/java/com/app/controller/GitHubController.java
+++ simple-java-webapp-containerized/src/main/java/com/app/controller/GitHubController.java
@@ -2,10 +2,13 @@ package com.app.controller;
 
 import com.app.service.GitHubService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/github")
@@ -16,7 +19,12 @@ public class GitHubController {
     private GitHubService gitHubService;
 
     @GetMapping("/repos/{username}")
-    public String[] getUserRepositories(@PathVariable String username) {
-        return gitHubService.getRepositories(username);
+    public ResponseEntity<List<String>> getUserRepositories(@PathVariable String username) {
+        List<String> repositories = gitHubService.getRepositories(username);
+        if (repositories.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(repositories);
+
     }
 }
--- simple-java-webapp-containerized/src/main/java/com/app/service/GitHubService.java
+++ simple-java-webapp-containerized/src/main/java/com/app/service/GitHubService.java
@@ -4,17 +4,32 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 @Service
 @AllArgsConstructor
 public class GitHubService {
 
     private static final String GITHUB_API_URL = "https://api.github.com/users/{username}/repos";
 
-
     private RestTemplate restTemplate;
 
-    public String[] getRepositories(String username) {
-        return restTemplate.getForObject(GITHUB_API_URL, String[].class, username);
+    public List<String> getRepositories(String username) {
+
+        Map<String, Object>[] response = restTemplate.getForObject(GITHUB_API_URL, Map[].class, username);
+        List<String> repositories = new ArrayList<>();
+
+        if (response != null) {
+            for (Map<String, Object> repo : response) {
+                String repoName = repo.get("name").toString();
+                repositories.add(repoName);
+            }
+        }
+
+        return repositories;
     }
 }
 
